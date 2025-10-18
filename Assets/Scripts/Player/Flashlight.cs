5567e22fcbdb4121cf15ce0e2da024b5eabf6fdb
puargamer <79729942+puargamer@users.noreply.github.com>
2025-10-18 03:42:14 -0700

    add player prefab
    add flashlight script

--- Assets/Scripts/Player/Flashlight.cs
+++ Assets/Scripts/Player/Flashlight.cs
@@ -1,7 +1,15 @@
+using System;
 using UnityEngine;
+
+//flashlight that activates on spacebar hold
 
 public class Flashlight : MonoBehaviour
 {
+    public Light flashlight;
+
+    [Header("State")]
+    [SerializeField] private bool isOn;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +19,22 @@ public class Flashlight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            isOn = true;
+        }
+        else 
+        { 
+            isOn = false;
+        }
+
+        if (isOn)
+        {
+            flashlight.enabled = true;
+        }
+        else
+        {
+            flashlight.enabled = false;
+        }
     }
 }
